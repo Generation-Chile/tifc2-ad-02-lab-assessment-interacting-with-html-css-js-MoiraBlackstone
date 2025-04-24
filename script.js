de710cd7efa44c3bb21682445fed800d7c4d5afe
MoiraBlackstone <moira.alexandra.cf@gmail.com>
2025-04-24 15:16:04 +0000

    tarea interactuando html css y js lista

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("titulo").textContent = "Adiós"
+document.getElementById("cinco").style.color = "orange"
+const h6 = document.getElementById("boton");
+
+h6.addEventListener("click", () => {
+  h6.style.color = "brown";
+});
